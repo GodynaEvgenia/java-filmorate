4befb17a73a3fe9eed3d4e3d91cc4b550288a298
GodynaEvgenia <evgeniagodyna@yandex.ru>
2025-07-10 14:41:33 +0300

    Очистка таблиц перед стартом приложения

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,47 +1,68 @@
-  CREATE TABLE IF NOT EXISTS genre(
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      name VARCHAR(50) NOT NULL,
-      description VARCHAR(255)
-  );
+DROP TABLE IF EXISTS FILM_GENRE;
 
-  CREATE TABLE IF NOT EXISTS rating(
-      id integer AUTO_INCREMENT PRIMARY KEY,
-      name VARCHAR(50) NOT NULL,
-      description VARCHAR(255)
-  );
+DROP TABLE IF EXISTS FRIENDSHIP;
 
-  CREATE TABLE IF NOT EXISTS films (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      description VARCHAR(255),
-      release_date DATE,
-      duration INT,
-      rating INT REFERENCES rating
-  );
+DROP TABLE IF EXISTS GENRE;
 
-  CREATE TABLE IF NOT EXISTS users(
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      name VARCHAR(100) NOT NULL,
-      login VARCHAR(50) NOT NULL,
-      email VARCHAR(100) NOT NULL,
-      birthday DATE
-  );
+DROP TABLE IF EXISTS LIKES;
 
-  CREATE TABLE IF NOT EXISTS likes (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      user_id INT NOT NULL REFERENCES users,
-      film_id INT NOT NULL REFERENCES films
-  );
+DROP TABLE IF EXISTS FILMS;
 
-  CREATE TABLE IF NOT EXISTS friendship(
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      user_id INT NOT NULL REFERENCES users,
-      friend_id INT NOT NULL REFERENCES users,
-      status VARCHAR(50)
-  );
+DROP TABLE IF EXISTS RATING;
 
-  CREATE TABLE IF NOT EXISTS film_genre(
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      film_id INT NOT NULL REFERENCES films,
-      genre_id INT NOT NULL REFERENCES genre
-  );
+DROP TABLE IF EXISTS USERS;
+
+CREATE TABLE genre
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(50) NOT NULL,
+    description VARCHAR(255)
+);
+
+CREATE TABLE rating
+(
+    id          integer AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(50) NOT NULL,
+    description VARCHAR(255)
+);
+
+CREATE TABLE films
+(
+    id           INT AUTO_INCREMENT PRIMARY KEY,
+    name         VARCHAR(255) NOT NULL,
+    description  VARCHAR(255),
+    release_date DATE,
+    duration     INT,
+    rating       INT REFERENCES rating
+);
+
+CREATE TABLE users
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    name     VARCHAR(100) NOT NULL,
+    login    VARCHAR(50)  NOT NULL,
+    email    VARCHAR(100) NOT NULL,
+    birthday DATE
+);
+
+CREATE TABLE likes
+(
+    id      INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users,
+    film_id INT NOT NULL REFERENCES films
+);
+
+CREATE TABLE friendship
+(
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    user_id   INT NOT NULL REFERENCES users,
+    friend_id INT NOT NULL REFERENCES users,
+    status    VARCHAR(50)
+);
+
+CREATE TABLE film_genre
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    film_id  INT NOT NULL REFERENCES films,
+    genre_id INT NOT NULL REFERENCES genre
+);
